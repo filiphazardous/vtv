73333acea8e1622fbf4a559ca5d228b756584975
Filip Joelsson <filip@pixiecoder.com>
2017-06-28 21:41:55 +0200

    Added md to html conversion

--- build-utils/section.js
+++ build-utils/section.js
@@ -1,12 +1,14 @@
 "use strict";
 
 export function convertToSectionAr(content, lang) {
+  const showdown = require('showdown');
+  const converter = new showdown.Converter();
   return content.items
     .filter(i => i.sys.contentType.sys.id === 'section' && `/${i.fields.language}/` === lang)
     .map(i => {
       return {
         title: i.fields.title,
-        body: i.fields.body,
+        body: converter.makeHtml(i.fields.body),
         media: `${i.fields.media.sys.id}.jpg`,
         mediaCaption: i.fields.mediaCaption,
         key: i.fields.key,
